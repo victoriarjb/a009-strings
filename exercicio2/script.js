9d210eb580eb2f0249e83b17ef2cc0fe2597cb0c
victoriarjb <victoriarjbarbosa67@gmail.com>
2022-08-31 19:19:41 -0300

    meu primeiro commit

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,14 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString1 = "Eu não sou supersticioso, mas sou um pouco ________.      "
+console.log (`Frase inicial:
+${minhaString1}`)
+console.log (`Removendo o excesso de espaços:
+${minhaString1.trim()}`)
+
+const minhaString2 = minhaString1.trim()
+
+console.log (` Quantidade de caracteres antes x depois:
+A frase inicial possui ${minhaString1.length} caracteres. Após a remoção dos espaços, a frase passou a ter ${minhaString2.length} caracteres.`)
+
+console.log (`Substituindo os traços:
+${minhaString1.trim().replace("________", "“sticioso”")}`)
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,14 @@
-//Crie a const para a frase aqui
+const frase = ('Jorge tem uma casa verde e com portão azul, com os dizeres: "\BOAS VINDAS, mas não deixe o gato sair\"') //Crie a const para a frase aqui
+console.log (frase)
+
+const frase2 = frase.replace("verde","rosa").replace("azul","laranja")
+console.log (frase2)
+
+console.log (`Verificação se a nova frase inclui
+verde: ${frase2.includes("verde")}
+laranja: ${frase2.includes("laranja")}`)
+
+const trechoDaFrase = ('Jorge tem uma casa verde e com portão azul, com os dizeres: "\BOAS VINDAS,')
+const trechoDaFrase2 = ('mas não deixe o gato sair\"')
+const trechoDaFrase2ToUpperCase = trechoDaFrase2.toUpperCase()
+console.log (`${trechoDaFrase} ${trechoDaFrase2ToUpperCase}`)
